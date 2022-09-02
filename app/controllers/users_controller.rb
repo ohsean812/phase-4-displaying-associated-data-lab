98f7666e425cacfea9292a2ce187774d75808a9a
Sean Oh <ohsean812@gmail.com>
2022-09-02 12:54:14 -0400

    commit tree 2823ba7c446e3f7203051c3200aefc2c3d043cbf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
+
+    
+private
+
+    def user_params
+        params.permit(:username, :city)
+    end
+
 end
